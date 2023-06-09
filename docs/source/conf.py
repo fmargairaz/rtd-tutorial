16633876f7199273f9b1a463b0e21f048f90cbec
Fabien Margairaz <fabien.margairaz@utah.edu>
2023-06-09 14:01:14 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'QES'
 copyright = '2023, University of Utah'
 author = 'Fabien Margairaz'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
